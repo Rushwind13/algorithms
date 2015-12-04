8bd228fad30ae2311fd7ce2e0d7b2e21709c69a7
Jimbo Harris (jiharris) <jiharris@expedia.com>
2015-12-04 12:10:30 -0800

    sorted insertion works

--- src/main/java/jimbo/mike/LinkedList.java
+++ src/main/java/jimbo/mike/LinkedList.java
@@ -21,28 +21,25 @@ public class LinkedList
 
 	public void insert( Node after )
 	{
-             Node curr = this.head;
-	     if( this.head == null )
-             {
-                this.head = after;
-		return;
-	     }
+		// special case: if list empty or head larger than incoming, incoming is new head
+		if( this.head == null || this.head.compareTo(after) > 0 )
+		{
+			// after is the new head
+			after.next = head;
+			head = after;
+			return;
+		}
 
-
-	     if( this.head.next == null && this.head.compareTo(after) > 0)
-	     {
-	        after.next = head;
-		head = after; 
-		return;
- 	     }
-
-	     while( curr.next != null && curr.compareTo(after) < 0 ){
-		curr = curr.next;
-	     }			
-	     after.next = curr.next;
-	     curr.next = after;
+		// Not empty list, not left of list, so somewhere in middle.
+		Node curr = head;
+		while( curr.next != null && curr.next.compareTo(after) < 0 )
+		{
+			curr = curr.next;	
+		}
+		// insert before curr_next
+		after.next = curr.next;
+		curr.next = after;
 	}
-
 	public String toString()
 	{
 		StringBuffer retval = new StringBuffer();
@@ -59,7 +56,7 @@ public class LinkedList
     {
 	LinkedList theList = new LinkedList();
 	theList.insert( "1" );
-	theList.insert( "4" );
+	//theList.insert( "4" );
 
         System.out.println( theList );
 
@@ -67,13 +64,21 @@ public class LinkedList
 	n2.insert("2");
         theList.insert(n2.head);
 
-	LinkedList n0 = new LinkedList();
+	/**/LinkedList n0 = new LinkedList();
 	n0.insert("0");
         theList.insert(n0.head);
 
 	LinkedList n5 = new LinkedList();
 	n5.insert("5");
-        theList.insert(n5.head);
+        theList.insert(n5.head);/**/
+	LinkedList n9 = new LinkedList();
+	n9.insert("9");
+        theList.insert(n9.head);/**/
+	LinkedList n3 = new LinkedList();
+	n3.insert("3");
+        theList.insert(n3.head);/**/
+
+
         //theList.insert(new LinkedList.Node( "0" ));
         //theList.insert(new LinkedList.Node( "3" ));
         //theList.insert(new LinkedList.Node( "5" ));
